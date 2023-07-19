849b324dc8ba0b16ae84e51ab4585d7d258a55ed
Sanketchaudhari1998 <csanket18@gmail.com>
2023-07-19 13:52:48 +0530

    commit changes with variable value and datatype
    commit changes with variable value and datatype

---
+++ 01_variableAssign.js
@@ -0,0 +1,31 @@
+var myFullName;
+console.log(myFullName);
+myFullName = "Sanket Chaudhari";
+console.log(myFullName);
+myFullName = "Sanket Hari Chaudhari";
+console.log(myFullName);
+
+var myLove;
+myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Sanket";
+console.log(firstName);
+
+var lastName = "Chaudhari";
+console.log(lastName);
+
+var age = 25;
+console.log(age);
+
+var address = "Jalgaon";
+console.log(address);
+
+var pinCode = 425502;
+console.log(pinCode);
+
+var gender = "Male"
+console.log(gender);
+
+var isMarried = false;
+console.log(isMarried);
