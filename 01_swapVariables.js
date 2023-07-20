6015addcd8a12586711247624e262f9ca9a824c7
Sanketchaudhari1998 <csanket18@gmail.com>
2023-07-20 14:06:51 +0530

    swapping variables
    swapping variables

---
+++ 01_swapVariables.js
@@ -0,0 +1,18 @@
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("Before swap --->     sweety-->", sweety, " cutie-->", cutie);
+var temp = "Sweety"
+var sweety = "Cutie";
+var cutie = "Sweety";
+console.log("After swap --->     sweety-->", sweety, " cutie-->", cutie);
+console.log("------------------------------------------------------------------------");
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("Before swap --->   num1 =", num1, " num2 =", num2, " num3 =", num3);
+var temp = 100;
+var num1 = 200;
+var num2 = 300;
+var num3 = 100;
+console.log("After swap --->   num1 =", num1, " num2 =", num2, " num3 =", num3);
