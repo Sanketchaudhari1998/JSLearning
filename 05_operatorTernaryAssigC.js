c82ace1f45e6dc8fd16ab13abe090a86e8a5331d
Sanketchaudhari1998 <csanket18@gmail.com>
2023-07-26 17:08:19 +0530

    used ternary operator for given task
    used ternary operator for given task

---
+++ 05_operatorTernaryAssigC.js
@@ -0,0 +1,19 @@
+console.log(`-------------------------- TASK 1 ----------------------------`);
+function maleMarriageEligibility(gender, age, boyName) {
+  var boyEligibility = (gender = "Male" && age >= 21)
+    ? "You are eligible for marriage"
+    : "You are not eligible for marriage";
+    console.log(`Hey ${boyName}, ${boyEligibility}`);
+}
+maleMarriageEligibility("Male", 25, "Bill Gates");
+maleMarriageEligibility("Male", 17, "Stew Jobs");
+
+console.log(`-------------------------- TASK 2 ----------------------------`);
+function girlMarriageEligibility(gender , age , girlName){
+    var girlEligibility = (gender = "Female" && age >= 18)
+    ? "You are eligible for marriage"
+    : "You are not eligible for marriage";
+    console.log(`Hey ${girlName}, ${girlEligibility}`);
+}
+girlMarriageEligibility("Female" , 16 , "Jenifer")
+girlMarriageEligibility("Female" , 27 , "Malinda Gates")
