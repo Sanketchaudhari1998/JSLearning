60a30b5876532c1cee968131641569a74ead10fa
Sanketchaudhari1998 <csanket18@gmail.com>
2023-07-31 14:37:06 +0530

    While loop Assignment02
    While loop Assignment02

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,50 @@
+console.log(
+  `------ Step 1 : Print numbers from 5 to 15 by incrementing 1--------`
+);
+var index = 5;
+while (index <= 15) {
+  console.log(index);
+  index = index + 1
+}
+
+console.log(`------- Step 2 : Print numbers from 50 to 40 by decrementing 1--------`);
+var index = 50
+while (index >=40) {
+    console.log(index);
+    index = index-1
+}
+
+console.log(`------ Step 3 : WAP to find first 15 odd numbers----------`);
+var index = 1
+while (index < 30) {
+    console.log(index);
+    index = index +2
+}
+
+console.log(`------ Step 4 : WAP to find first 10 even numbers------`);
+var index = 0
+while (index < 20) {
+    console.log(index);
+    index = index +2
+}
+
+console.log(`------ Step 5 : WAP to print table of 5 -------`);
+var index = 5
+while (index <= 50) {
+    console.log(index);
+    index = index +5
+}
+
+console.log(`------ Step 6 : WAP to print table of 10 ------- `);
+var index = 10
+while (index <= 100) {
+    console.log(index);
+    index = index +10
+}
+
+console.log(`------ Step 7 : WAP to print table of 10 in reverse order ------- `);
+var index = 100
+while (index >= 10) {
+    console.log(index);
+    index = index -10
+}
