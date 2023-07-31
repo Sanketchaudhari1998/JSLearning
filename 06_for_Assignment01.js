6cbdeacbe7f1ae2b9ffb87f6c8f33e1d1be67352
Sanketchaudhari1998 <csanket18@gmail.com>
2023-07-31 14:15:19 +0530

    For loop  Assignment 01
    For loop  Assignment 01

--- 06_for_Assignment01.js
+++ 06_for_Assignment01.js
@@ -1,40 +1,40 @@
-console.log(`-------------- Step 1 ------------------`);
+console.log(`------ Step 1 : Print numbers from 5 to 15 by incrementing 1--------`);
 for (let index = 5; index <= 15; index++) {
     
     console.log(index);
 }
 
-console.log(`-------------- Step 2 ------------------`);
+console.log(`------- Step 2 : Print numbers from 50 to 40 by decrementing 1--------`);
 for (let index = 50; index >= 40; index--) {
     
     console.log(index);
 }
 
-console.log(`-------------- Step 3 ------------------`);
+console.log(`------ Step 3 : WAP to find first 15 odd numbers----------`);
 for (let index = 1; index <= 30; index= index+2) {
     
     console.log(index);
 }
 
-console.log(`-------------- Step 4 ------------------`);
+console.log(`------ Step 4 : WAP to find first 10 even numbers------`);
 for (let index = 0; index < 20; index = index+2) {
     
     console.log(index);
 }
 
-console.log(`-------------- Step 5 ------------------`);
+console.log(`------ Step 5 : WAP to print table of 5 -------`);
 for (let index = 5; index <= 50; index= index+5) {
     
     console.log(index);
 }
 
-console.log(`-------------- Step 6 ------------------`);
+console.log(`------ Step 6 : WAP to print table of 10 ------- `);
 for (let index = 10; index <= 100; index=index+10) {
     
     console.log(index);
 }
 
-console.log(`-------------- Step 7 ------------------`);
+console.log(`------ Step 7 : WAP to print table of 10 in reverse order ------- `);
 for (let index = 100; index >= 10; index=index-10) {
     
     console.log(index);
