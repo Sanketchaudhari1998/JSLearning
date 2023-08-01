1ba14af6bf79b5f3239bdb6530674fd3101fef9e
Sanketchaudhari1998 <csanket18@gmail.com>
2023-08-01 15:40:39 +0530

    Reverse string and ignore spaces Assignment B
    Reverse string and ignore spaces Assignment B

---
+++ 06_reverseStrAndIngoreSpaces.js
@@ -0,0 +1,15 @@
+console.log(`===== reverse string and ignore spaces =====`);
+function reverseString(str) {
+    let reverse = ""
+    for (let index = str.length - 1; index >=0; index--) {
+        const char = str.charAt(index)
+        if (char != " ") {
+            reverse=reverse.concat(char)
+        }
+        
+    }
+    console.log(`${reverse}`);
+}
+
+reverseString("Hard work always pays back")
+reverseString("Soon I will be Angular IT Champ")
