11eb135fa5f3d29320006778b44a6bc6f276169e
Sanketchaudhari1998 <csanket18@gmail.com>
2023-08-21 12:55:11 +0530

    include or modified another way to get similar output
    include or modified another way to get similar output

--- 16_sumByUsingRestParameter.js
+++ 16_sumByUsingRestParameter.js
@@ -13,3 +13,20 @@ function add(...num ){
   add(9, 5, 6);
   add(78, 90, 51, 68);
   add(89, 90, 45,78, 90, 51, 68);
+
+// another approach or way to solve above code
+console.log("another approach or way to solve above code showing similar result");
+
+  function add(num1,num2,...num3 ){
+    let sum2 = num1+num2
+    for (let index = 0; index < num3.length; index++) {
+        let element = num3[index];
+        sum2 = sum2 + element
+        
+    }
+    console.log(`Sum of all elements in array is ${sum2}`);
+  }
+  add(5, 6);
+  add(9, 5, 6);
+  add(78, 90, 51, 68);
+  add(89, 90, 45,78, 90, 51, 68);
