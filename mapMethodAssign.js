58729229762f164910c7130501220f457915a484
Sanketchaudhari1998 <csanket18@gmail.com>
2023-09-05 21:28:52 +0530

    map method Assignment
    map method Assignment

---
+++ mapMethodAssign.js
@@ -0,0 +1,16 @@
+console.log(`----------------- adding 10 in every element-------------------`);
+const array_numbers = [ 20, 11, 40, 25, 23, 11, 9, 31, 60, 2, 19 ];
+let newarray = array_numbers.map((element)=>{
+return element+10
+});
+console.log(newarray);
+console.log(`----------------- Square of every element-------------------`);
+let square = array_numbers.map((element)=>{
+return element*element
+});
+console.log(square);
+console.log(`----------------- add index of element in element-------------------`);
+let addIndex = array_numbers.map((element,index)=>{
+return element+index
+});
+console.log(addIndex);
